199ee1e8078b4c6b020d18e4dfabb0b50c4b6934
AbdulrahmanMaktabi <abdurahmanmaktabi@gmail.com>
2024-10-15 18:44:24 +0300

    some edit on login system

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,9 +1,9 @@
 @props(['messages'])
 
 @if ($messages)
-    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }}>
+    <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 space-y-1']) }} style="list-style:none; padding: 0px">
         @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+            <li style="color: red">{{ $message }}</li>
         @endforeach
     </ul>
 @endif
